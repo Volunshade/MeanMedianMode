92390ea578b591623098e67cb7ae406d88d149ab
Thomas Newcomb <volunshade@gmail.com>
2022-07-20 20:25:17 -0700

    Created the functionality of finding the mode and created functionailty to solve a dataset.

--- mean_median_mode.py
+++ mean_median_mode.py
@@ -28,17 +28,48 @@ def calc_median(nums):
     return round(float(median), 2)
 
 
+def calc_mode(nums):
+    """Calculates the mode of a list of numbers"""
+
+    frequencies = {}
+    for unique_number in set(nums):
+        frequency = nums.count(unique_number)
+        frequencies[unique_number] = frequency
+
+    highest = -1
+    for value in frequencies.values():
+        if value > highest:
+            highest = value
+
+    modes = []
+    for key, value in frequencies.items():
+        if value == highest:
+            modes.append(key)
+
+    return modes
+
+
+def solve_dataset(dataset):
+    """Calculates the mean, median, and mode of a dataset"""
+
+    mean = calc_mean(dataset)
+    median = calc_median(dataset)
+    mode = calc_mode(dataset)
+
+    return mean, median, mode
+
+
 def main():
     """Runs the main process."""
 
     data_set_one = [6, 9, 1, 2, 6, 3, 7]
-    # data_set_two = [6, 9, 1, 2, 6, 3, 7, 4]
-    # data_set_three = [6, 9, 1, 2, 6, 3, 7, 4, 1]
+    data_set_two = [6, 9, 1, 2, 6, 3, 7, 4]
+    data_set_three = [6, 9, 1, 2, 6, 3, 7, 4, 1]
 
-    mean = calc_mean(data_set_one)
-    print(mean)
-    median = calc_median(data_set_one)
-    print(median)
+    datasets = (data_set_one, data_set_two, data_set_three)
+    for dataset in datasets:
+        mean, median, mode = solve_dataset(dataset)
+        print(f'{mean=}, {median=}, {mode=}')
 
 
 if __name__ == "__main__":
